9d7b51b9d7ba693ee1a8b580c1f374ad467edb35
t1t1 <cha@hanmail2.net>
2018-04-05 17:55:13 +0900

    #3 Add sqlite3, sequelize

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('sqlite:sample.db');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get('/', function(req, res) {
     res.send('Hello World');
