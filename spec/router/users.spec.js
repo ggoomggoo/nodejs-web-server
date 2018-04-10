ca96ba9f7d89b4e76a95e911e3608d803a3baf36
t1t1 <cha@hanmail2.net>
2018-04-10 15:05:01 +0900

    #7 Update test code to async

--- spec/router/users.spec.js
+++ spec/router/users.spec.js
@@ -1,7 +1,8 @@
 const should = require('should');
 
 describe('users api', () => {
-    it('true is true', () => {
+    it('true is true', (done) => {
         (true).should.be.equal(true);
+        done();
     });
 });
