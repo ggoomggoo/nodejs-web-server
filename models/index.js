c69c625a00ce5125643a100508a6d7f0df99087b
t1t1 <cha@hanmail2.net>
2018-04-11 14:14:26 +0900

    #7 Update NODE_ENV options to [“local”, “dev”, “prod”]

--- models/index.js
+++ models/index.js
@@ -4,16 +4,10 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'local';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
-let sequelize;
-
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
     .readdirSync(__dirname)
